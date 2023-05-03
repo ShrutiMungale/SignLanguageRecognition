a7d796237a182de70eeed67da7c671d935912adc
ShrutiMungale <shruti020810@gmail.com>
2023-05-04 00:11:02 +0530

    commit

---
+++ trainmodel.py
@@ -0,0 +1,72 @@
+from function import *
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+from keras.callbacks import TensorBoard
+label_map = {label:num for num, label in enumerate(actions)}
+# print(label_map)
+sequences, labels = [], []
+for action in actions:
+    for sequence in range(no_sequences):
+        window = []
+        for frame_num in range(sequence_length):
+            res = np.load(os.path.join(DATA_PATH, action, str(sequence), "{}.npy".format(frame_num)))
+            window.append(res)
+        sequences.append(window)
+        labels.append(label_map[action])
+
+
+
+
+
+X = np.array(sequences)
+y = to_categorical(labels).astype(int)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
+
+log_dir = os.path.join('Logs')
+tb_callback = TensorBoard(log_dir=log_dir)
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(15,63)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))
+#
+
+
+
+
+# # Import confusion matrix function
+# from sklearn.metrics import confusion_matrix
+
+# # Make predictions on the test data
+# y_pred = model.predict(X_test)
+
+# # Convert the predicted probabilities into class labels
+# y_pred = np.argmax(y_pred, axis=1)
+
+# # Convert the one-hot encoded true labels into class labels
+# y_true = np.argmax(y_test, axis=1)
+
+# # Generate the confusion matrix
+# cm = confusion_matrix(y_true, y_pred)
+
+# # Print the confusion matrix
+# print(cm)
+
+
+
+
+
+
+
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+model.fit(X_train, y_train, epochs=500, callbacks=[tb_callback])
+model.summary()
+
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+model.save('model.h5')
